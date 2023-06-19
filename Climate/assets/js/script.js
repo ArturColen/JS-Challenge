2871c96713d64035b9e260a7e099ba7b4e872fb0
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-06-19 09:48:37 -0300

    Update script.js

--- Climate/assets/js/script.js
+++ Climate/assets/js/script.js
@@ -1,20 +1,15 @@
 document.querySelector('#search').addEventListener('submit', async (event) => {
-    // Prevent form submission
     event.preventDefault();
 
-    // Store the data entered by the user
     let input = document.querySelector('#search-input').value;
 
     if (input !== '') {
         clearInfo();
         showWarning('Carregando...');
 
-        // Store the request result
         let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=ff8336ebe0ee55dff31a03f12f1f6731&units=metric&lang=pt_br`);
-        // Transform the result of the request to json
         let json = await results.json();
 
-        // Check if the city was found in the request
         if (json.cod == 200) {
             showInfo({
                 name: json.name,
@@ -35,7 +30,6 @@ document.querySelector('#search').addEventListener('submit', async (event) => {
     }
 }); 
 
-// Show the information to the user
 function showInfo(json) {
     showWarning('');
 
@@ -47,13 +41,11 @@ function showInfo(json) {
     document.querySelector('#result').style.display = 'block';
 }
 
-// Hide the result field when searching for the city
 function clearInfo() {
     showWarning('');
     document.querySelector('#result').style.display = 'none';
 }
 
-// Displaying a warn message to the user
 function showWarning(message) {
     document.querySelector('#warning').innerHTML = message;
 }
